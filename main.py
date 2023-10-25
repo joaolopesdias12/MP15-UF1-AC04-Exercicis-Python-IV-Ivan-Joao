70aabf620fb04f5f5999f9b1df012fa346d7950a
Ivan Martinez Cañero <ivan.martinez.7e6@itb.cat>
2023-10-25 17:40:58 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("hello world")
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+
+# Estacions
+with open('CSV/2020_MeteoCat_Estacions.csv') as csvfile:
+    ndarray_estacions = []
+    dades = csv.reader(csvfile)
+    for i, fila in enumerate(dades):
+        ndarray_estacions.append(fila)
+
+# Detall Estacions
+with open('CSV/2022_MeteoCat_Detall_Estacions.csv') as csvfile:
+    ndarray_detall_estacions = []
+    dades = csv.reader(csvfile)
+    for i, fila in enumerate(dades):
+        ndarray_detall_estacions.append(fila)
+
+# Metadades
+with open('CSV/MeteoCat_Metadades.csv') as csvfile:
+    ndarray_metadades = []
+    dades = csv.reader(csvfile)
+    for i, fila in enumerate(dades):
+        ndarray_metadades.append(fila)
+
+print(ndarray_estacions)
