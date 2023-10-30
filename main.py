58dcd55898bc8e062bf3faf7295aa47c9724822d
Ivan Martinez Cañero <ivan.martinez.7e6@itb.cat>
2023-10-30 19:55:32 +0100

    Predict temp mitjana 2023

--- main.py
+++ main.py
@@ -41,13 +41,38 @@ def temp_mitjana_febrer(estacions: numpy.ndarray, detall_estacions: numpy.ndarra
     # Create a numpy array from the list of mitjanes and create a plot
     print(numpy.array(mitjanes))
     plt.plot(numpy.array(mitjanes)[:,0], numpy.array(mitjanes)[:,1])
+    plt.ylabel("Temperatura (ºC)")
+    plt.xlabel("Dia")
     plt.xticks(range(1,29))
-    plt.title("Temperatura mitjana de febrer")
+    plt.title("Temperatura mitjana de febrer 2022")
     plt.show()
+    return mitjanes
                 
-    
+#print(temp_mitjana_febrer(estacions, detall_estacions, metadades))
 
+def predict_temp_mitjana_febrer_2023(estacions: numpy.ndarray, detall_estacions: numpy.ndarray, metadades: numpy.ndarray):
+    mitjanes = temp_mitjana_febrer(estacions, detall_estacions, metadades)
+# Supongamos que tienes una lista de temperaturas medias diarias para febrero de 2022
+    temperaturas_febrero_2022 = numpy.array(mitjanes)[:,1]
+    # Crear un histograma para visualizar la distribución de las temperaturas de febrero de 2022
+    plt.hist(temperaturas_febrero_2022, bins=range(-10, 26), edgecolor='black')
+    plt.xlabel('Temperatura (°C)')
+    plt.ylabel('Frecuencia')
+    plt.title('Distribución de temperaturas en febrero de 2022')
+    plt.show()
 
+    # Calcular la media y la desviación estándar de las temperaturas de febrero de 2022
+    media = numpy.mean(temperaturas_febrero_2022)
+    desviacion_estandar = numpy.std(temperaturas_febrero_2022)
 
+    # Generar valores aleatorios para febrero de 2023 basados en la distribución de 2022
+    temperaturas_febrero_2023 = numpy.random.normal(media, desviacion_estandar, 28)  # Suponiendo 28 días en febrero
 
-print(temp_mitjana_febrer(estacions, detall_estacions, metadades))
+    # Graficar la distribución de las temperaturas de febrero de 2023
+    plt.hist(temperaturas_febrero_2023, bins=range(-10, 26), edgecolor='black')
+    plt.xlabel('Temperatura (°C)')
+    plt.ylabel('Frecuencia')
+    plt.title('Distribución de temperaturas en febrero de 2023')
+    plt.show()
+
+print(predict_temp_mitjana_febrer_2023(estacions, detall_estacions, metadades))
