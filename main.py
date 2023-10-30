f03381e8e6994b9fc14844b1a7b04f6cf6d895a0
Ivan Martinez Cañero <ivan.martinez.7e6@itb.cat>
2023-10-30 19:45:26 +0100

    Temp mitjana febrer

--- main.py
+++ main.py
@@ -2,45 +2,52 @@ import matplotlib.pyplot as plt
 import numpy
 import csv
 
+# Exercici 2: Carregar dades
+def getMatrixFromFile(fileName):
+    file = open(fileName, 'r')
+    reader = csv.reader(file)
+    header = next(reader)  # skip the header row if it exists
+    myList = [ row for row in reader]
+    ndarray = numpy.array(list(myList))
+    file.close()
+    return ndarray
 
-# Estacions
-with open('CSV/2020_MeteoCat_Estacions.csv') as csvfile_estacions:
-    dades_estacions = list(csv.DictReader(csvfile_estacions))
-    list_dades = list(dades_estacions)
-    ndarray_estacions = []
-    for fila in list_dades:
-        ndarray_estacions.append(fila)
-    ndarray_estacions = numpy.array(ndarray_estacions)
+estacions = getMatrixFromFile('CSV/2020_MeteoCat_Estacions.csv')
+detall_estacions = getMatrixFromFile('CSV/2022_MeteoCat_Detall_Estacions.csv')
+metadades = getMatrixFromFile('CSV/MeteoCat_Metadades.csv')
+print(estacions.shape)
+print(detall_estacions.shape)
+print(metadades.shape)
 
-# Detall Estacions
-with open('CSV/2022_MeteoCat_Detall_Estacions.csv') as csvfile_detall_estacions:
-    dades_detall = list(csv.DictReader(csvfile_detall_estacions))
-
-    ndarray_detall_estacions = list()
-    for fila in dades_detall:
-        ndarray_detall_estacions.append(fila)
-    ndarray_detall_estacions = numpy.array(ndarray_detall_estacions)
-
-# Metadades
-with open('CSV/MeteoCat_Metadades.csv') as csvfile_metadades:
-    dades_metadades = list(csv.DictReader(csvfile_metadades))
-    ndarray_metadades = list()
-    for fila in dades_metadades:
-        ndarray_metadades.append(fila)
-    ndarray_metadades = numpy.array(ndarray_metadades)
-
-
-#print(ndarray_estacions)
-# print forma of matrix
-#print("hola")
-print(ndarray_estacions[0]["CODI_ESTACIO"])
-print(ndarray_estacions.shape)
-
-def temp_mitjana_febrer(ndarray_estacions: list, ndarray_detall_estacions: list, ndarray_metadades: list):
-    dies = numpy.array([i for i in range(1, 29)])
-    #print(ndarray_detall_estacions)
-    print(dies)
+# Exercici 3: Temperatura mitjana de febrer
+def temp_mitjana_febrer(estacions: numpy.ndarray, detall_estacions: numpy.ndarray, metadades: numpy.ndarray):
+    dies_temperatures = {}
+    for row in detall_estacions:
+        data = row[0]
+        tm = row[3]
+        dia = data.split("-")[2]
+        mes = data.split("-")[1]
+        # Relate days with average temperature
+        if mes == "02" and tm == "TM":
+            dia_int = int(dia)
+            temperatura = float(row[4])
+            if dia_int not in dies_temperatures:
+                dies_temperatures[dia_int] = []
+            dies_temperatures[dia_int].append(temperatura)
+    mitjanes = []
+    for dia, temperatures in dies_temperatures.items():
+        mitjana = sum(temperatures) / len(temperatures)
+        mitjanes.append((dia, mitjana))
+    # Create a numpy array from the list of mitjanes and create a plot
+    print(numpy.array(mitjanes))
+    plt.plot(numpy.array(mitjanes)[:,0], numpy.array(mitjanes)[:,1])
+    plt.xticks(range(1,29))
+    plt.title("Temperatura mitjana de febrer")
+    plt.show()
+                
+    
 
 
 
-temp_mitjana_febrer(ndarray_estacions, ndarray_detall_estacions, ndarray_metadades)
+
+print(temp_mitjana_febrer(estacions, detall_estacions, metadades))
